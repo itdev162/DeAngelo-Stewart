cc98029d48fd424a9773d46c6b77fdfc1a698720
Malabeadman <deangelo.lee19@gmail.com>
2021-09-29 19:12:01 -0500

    Completed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
+import axios from 'axios';
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    values: []
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+
+      this.setState({
+        values: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
   render() {
     return (
     <div className="App">
       <header className="App-header">
-        Blogbox
+        BlogBox
       </header>
+        {this.state.values.map((value: any) => <div key= {value}>{value}</div>)}
     </div>
     );
   }
