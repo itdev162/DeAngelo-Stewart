0fed873d887bc2a61ffb7bf2d3c0436d72f6a17c
Malabeadman <deangelo.lee19@gmail.com>
2021-10-06 21:41:41 -0500

    Post table added and data added

--- Domain/Post.cs
+++ Domain/Post.cs
@@ -7,12 +7,12 @@ namespace Domain
 {
     public class Post
     {
-        public Guid Id {get; set;}
+        public Guid Id { get; set; }
 
-        public string Title {get; set;}
+        public string Title { get; set; }
 
-        public string Body {get; set;}
+        public string Body { get; set; }
 
-        public DateTime Date {get; set;}
+        public DateTime Date { get; set; }
     }
 }
